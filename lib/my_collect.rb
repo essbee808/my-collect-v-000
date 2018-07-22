d01521a0730024c38c954bd9a6eb5f21c6f70c3d
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-22 04:20:31 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,12 +2,12 @@ array = Array.new
 
 def my_collect(array)
   i = 0
-  modified_collection = []
+  collection = []
   while i < array.length
     modifield_collection << yield(array[i])
     i += 1
   end
-  modified_collection
+  collection
 end
 
 my_collect(array) {|item| "#{item}"}
