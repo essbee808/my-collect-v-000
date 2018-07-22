450378fe1a5c571eeee14d1b55c27fe1ae423513
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-22 03:32:19 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+languages = Array.new
 
+def my_collect(collection)
+  i = 0
+  capital_languages = Array.new
+  
+  if i < collection.length
+    capital_languages << yield(language)
+    i += 1
+  end
+  updated_collection
+end
 
+my_collect(languages) do |language|
+  language
+end
